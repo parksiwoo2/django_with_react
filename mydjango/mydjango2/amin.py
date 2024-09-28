a4086f232a7b8fee76d1ce93c79ac74e66e19092
parksiwoo2 <132458293+parksiwoo2@users.noreply.github.com>
2024-09-28 08:08:29 +0000

    09/28

--- mydjango/mydjango2/amin.py
+++ mydjango/mydjango2/amin.py
@@ -1,13 +1,26 @@
 import sqlite3
 query="악뮤"
 print("검색어:",query)
+
 connection=sqlite3.connect("melon-20230906.sqlite3")
+#파일 경로를 인자로 받아서 연결, 트랜잭션 관리 담당
 cursor=connection.cursor()
-cursor.execute("SELECT*FROM songs")
+#커서를 통해 쿼리 실행
+connection.set_trace_callback(print)
+
+param='%'+query+'%'
+sql=f"SELECT * FROM songs WHERE 가수 LIKE ? OR 곡명 LIKE ?"
+cursor.execute(sql,[param,param])
+#select 쿼리 -> 요청만 전송되고(쿼리 실행) 아직 메모리에 가져온게 아님
 column_names=[desc[0] for desc in cursor.description]
+#메타데이터(데이터를 설명하는 데이터(데이터 구조,관계 등))를 가져옴
 song_list=cursor.fetchall()
+#전체행 가져오기 (파이썬 메모리로 가져오는 것)
+#cursor.fetchone() -> 한 행만 가져오기
+print("list size :",len(song_list))
 for song_tuple in song_list:
     song_dict=dict(zip(column_names,song_tuple))
     #print(song_dict["곡명"],song_dict["가수"])
     print("{곡명}{가수}".format(**song_dict))
+    #키, 값을 언팩해서 메소드에 전달, 키를 통해 값에 접근 가능
 connection.close()
--- mydjango/mydjango2/mydjango2.py
+++ mydjango/mydjango2/mydjango2.py
@@ -7,10 +7,19 @@ from django.core.management import execute_from_command_line
 # from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import path
+from django.db import connection
+from django.db import models
+from django.db.models import Q
 settings.configure(
     ROOT_URLCONF=__name__,
     DEBUG=True,
     SECRET_KEY="secret",
+    DATABASES={
+        "default":{
+            "ENGINE":"django.db.backends.sqlite3",
+            "NAME":"melon-20230906.sqlite3",
+        },
+    },
     TEMPLATES=[
         {
             "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -19,26 +28,43 @@ settings.configure(
     ],
 )
 django.setup()
+
+class Song(models.Model):
+    id=models.AutoField(primary_key=True)
+    가수=models.CharField(max_length=100)
+    곡일련번호=models.CharField(max_length=100)
+    순위=models.IntegerField()
+    앨범=models.IntegerField()
+    좋아요=models.CharField(max_length=200)
+    커버이미지_주소=models.IntegerField()
+    곡명=models.URLField()
+    class Meta:
+        db_table="songs"
+        app_label="melon"
 def index(request):
     query=request.GET.get("query","").strip()
-    json_url = "https://raw.githubusercontent.com/pyhub-kr/dump-data/main/melon/melon-20230906.json"
-    response = requests.get(json_url)
-    response.raise_for_status()
-    
-    song_list=response.json()
-    """song_list = [
-         {"곡명": "Seven (feat. Latto) - Clean Ver.", "가수": "정국"},
-         {"곡명": "Love Lee", "가수": "AKMU (악뮤)"},
-         {"곡명": "Super Shy", "가수": "NewJeans"},
-         {"곡명": "후라이의 꿈", "가수": "AKMU (악뮤)"},
-         {"곡명": "어떻게 이별까지 사랑하겠어, 널 사랑하는 거지", "가수": "AKMU (악뮤)"},
-    ]"""
+    song_list=Song.objects.all() #query set
     if query:
-        song_list=filter(lambda song:query in song ["가수"],song_list)
-    # 파이썬 빌트인 함수 filter를 활용해서, 곡명에 검색어가 포함된 노래만 필터링
-    # song_list = filter(lambda song: query in song["가수"] or query in song["곡명"], song_list)
-    return render(request, "index.html", {"song_list": song_list, "query":query})
+        song_list=song_list.filter(
+            Q(곡명__icontains=query) | Q(가수__icontains=query)
+        )
+    song_list_data=list(song_list.values())
+    return render(request, "index.html", {"song_list_data": song_list_data, "query":query})
 urlpatterns = [
     path("", index),
 ]
+"""def get_song(query: str):
+    cursor=connection.cursor()
+    if query:
+        param='%'+query+'%'
+        sql=f"SELECT * FROM songs WHERE 가수 LIKE %s OR 곡명 LIKE %s"
+        cursor.execute(sql,[param,param])
+    else:
+        cursor.execute(f"SELECT * FROM songs")
+    column_names=[desc[0] for desc in cursor.description]
+    song_list=[]
+    for song_tuple in cursor.fetchall():
+        song_dict=dict(zip(column_names,song_tuple))
+        song_list.append(song_dict)
+    return song_list"""
 execute_from_command_line(sys.argv)
