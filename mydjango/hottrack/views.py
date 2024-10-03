9bb2ff07f06c2df81dc3f8089c2bc74c8b71c569
parksiwoo2 <132458293+parksiwoo2@users.noreply.github.com>
2024-10-03 06:44:37 +0000

    10/03 #2

--- mydjango/hottrack/views.py
+++ mydjango/hottrack/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+import json
+from urllib.request import urlopen
 from django.http import HttpRequest,HttpResponse
-# Create your views here.
+from django.shortcuts import render
+from hottrack.models import Song
 
-def index(request:HttpRequest)->HttpResponse:
-    return render(request, template_name="hottrack/index.html",context={})
-
-
-    
+def index(request: HttpRequest)-> HttpResponse:
+    melon_chart_url= "https://raw/githubusercontent.com/pyhub-kr/dump-data/main/melon-20230910.json"
+    json_string=urlopen(melon_chart_url).read().decode("utf-8")
+    song_list={
+        Song.from_dict(song_dict)
+        for song_dict in json.loads(json_string)
+    }
+    return render(
+        request=request,
+        template_name="hottrack/index.html",
+        context={
+            "song_list":song_list,
+        },
+    )
