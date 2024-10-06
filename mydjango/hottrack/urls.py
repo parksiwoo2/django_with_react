424580d6c620a4e9ec407b556dd1e1d4d2f81404
parksiwoo2 <132458293+parksiwoo2@users.noreply.github.com>
2024-10-06 05:39:30 +0000

    10/06 #2

--- mydjango/hottrack/urls.py
+++ mydjango/hottrack/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path(route="",view=views.index),
+    path(route="<int:pk>/cover.png",view=views.cover_png)
 ]
--- mydjango/hottrack/views.py
+++ mydjango/hottrack/views.py
@@ -1,10 +1,10 @@
 import json
 from urllib.request import urlopen
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from hottrack.models import Song
 from django.db.models import QuerySet, Q
-
+from hottrack.utils.cover import make_cover_image
 
 def index(request: HttpRequest) -> HttpResponse:
     query = request.GET.get("query", "").strip()
@@ -37,3 +37,15 @@ def index(request: HttpRequest) -> HttpResponse:
             "song_list": song_qs,
         },
     )
+def cover_png(request, pk):
+    # 최대값 512, 기본값 256
+    canvas_size = min(512, int(request.GET.get("size", 256)))
+    song = get_object_or_404(Song, pk=pk)
+    cover_image = make_cover_image(
+        song.cover_url, song.artist_name, canvas_size=canvas_size
+    )
+    # param fp : filename (str), pathlib.Path object or file object
+    # image.save("image.png")
+    response = HttpResponse(content_type="image/png")
+    cover_image.save(response, format="png")
+    return response
